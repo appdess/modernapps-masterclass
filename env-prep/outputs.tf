1380d06f61a61bc727ac6e5b285465a9403287f0
Chris Saunders <saundersch@vmware.com>
2020-04-29 00:51:49 -0400

    updated outputs
    redundant entries

--- env-prep/outputs.tf
+++ env-prep/outputs.tf
@@ -1,8 +1,3 @@
-output "db_address" {
-    value = aws_db_instance.masterclass-db.address
-    description = "Database Instance Address"
-}
-
 output "db_endpoint" {
     value = aws_db_instance.masterclass-db.endpoint 
     description = "Database Endpoint"
