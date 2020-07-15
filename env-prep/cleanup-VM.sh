dd00ca01d5f1b5ed35865fb696c1b8e989375258
Alexander Dess <adess@vmware.com>
2020-07-15 14:33:14 +0200

    postcreate

--- env-prep/cleanup-VM.sh
+++ env-prep/cleanup-VM.sh
@@ -12,3 +12,48 @@ rm -rf ~/.kube
 #
 # Remove History from bash
 history -c
+
+# Automation
+docker run --rm -it vmware/powerclicore 
+
+$VMCVMFolder = "Workloads"
+$VMCResourcePool = "Compute-ResourcePool"
+$VMCDatastore = "WorkloadDatastore”
+
+
+$NewVMName = ”photon-clone2"
+$VMTemplate = "photon-3-kube-v1.18.2-vmware.1"
+New-VM -Name $NewVMName -Template (Get-Template $VMTemplate) -Location (Get-Folder $VMCVMFolder) -ResourcePool (Get-ResourcePool $VMCResourcePool) -Datastore (Get-Datastore $VMCDatastore)
+
+
+Recommended Global variables 
+$apiToken="OrqoRHE5DIcIkJUdJiF49fzly1ejdrU0J8Ngkk7ciDk5sGeEtJRB2tZWuC2Ahemn"
+$OrgName=“VMC-SET-EMEA”
+$SDDCName=“TKG_WS_SDDC”
+
+Connect-Vmc -ApiToken $apiToken
+Connect-NSXTProxy -RefreshToken $apiToken -OrgName $OrgName -SDDCName $SDDCName
+
+Get-NSXTNatRule
+Get-NSXTPublicIP
+Get-NSXTInfraGroup
+Get-NSXTSegment
+
+Managing FW-Rules with Powershell
+
+Get-NSXTDistFirewall -SectionName "WS-Test" | ft
+
+View the “MGW” or “CGW” NSXTGroups
+Get-NSXTGroup –GatewayType CGW | ft 
+
+
+New-NSXTDistFirewall -Name ”Deny-NSX-T" -Section "WS-Test” `
+-SourceGroup "Any" `
+        -DestinationGroup "TitoFE_IP" `
+        -Service http -Logged $true `
+        -SequenceNumber 5 `
+        -Action DROP
+
+
+        IuIfVuHBu9T5+!d
+
